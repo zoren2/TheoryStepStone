8eabbbd0986db7d69e960c5fa98dc0835dc76f38
Jack Tsang <cic_tian2@yahoo.com>
2021-12-09 14:59:20 -0800

    Added container for stream objects in core index.html javascript

--- test/javascript/browser-ndjson-stream-reader.js
+++ test/javascript/browser-ndjson-stream-reader.js
@@ -1,10 +1,12 @@
-    // Utility script written by Thibault Duplessis / ornicar@github
-    // https://gist.github.com/ornicar/a097406810939cf7be1df8ea30e94f3e
+    /*
+     * Utility script adapted by browser-ndjson-stream-reader by 
+     * Thibault Duplessis / ornicar@github
+     * https://gist.github.com/ornicar/a097406810939cf7be1df8ea30e94f3e
+     */ 
 
     //onMessage function is passed to readStream
     //onMessage is a callback function which just prints a line
     const readStream = processLine => response => {
-
         // 
         const stream = response.body.getReader();
         const matcher = /\r?\n/;
@@ -17,21 +19,26 @@
         const loop = () =>
             // read() returns a promise that resolves when a value has been received
             stream.read().then(
-
                 // Result objects contain two properties:
                 // done  - true if the stream has already given you all its data.
                 // value - some data. Always undefined when done is true.
                 ({ done, value }) => {
                 if (done) {
                     //
+                    console.log("inside done")
                     if (buf.length > 0) processLine(JSON.parse(buf));
+                    console.log("after buf")
                 }      
 
                 // Not finished, populate the buffer with stream content
                 else {
+                    console.log("inside else")
+                    
                     const chunk = decoder.decode(value, {
                         stream: true
                     });
+                    
+                    console.log(chunk);
                     
                     buf += chunk;
 
@@ -41,7 +48,6 @@
                     
                     // This segment
                     for (const i of parts.filter(p => p)) processLine(JSON.parse(i));
-
                     return loop();
                     }
             });
@@ -51,17 +57,22 @@
     }
 // const stream = fetch('https://lichess.org/api/tv/feed');
 // or any other ND-JSON endpoint such as:
-
+ 
 // fetch() returns a Promise that resolves to a Response Interface
-const stream = fetch('https://lichess.org/api/games/user/lovlas?max=5&moves=false',{headers:{Accept:'application/x-ndjson'}});
+const stream = fetch('https://lichess.org/api/games/user/lovlas?max=5',{headers:{Accept:'application/x-ndjson'}});
 console.log(stream);
-
+const games = new Array();
 // Callback function that simply logs a passed in object
-const onMessage = obj => console.log(obj);
+const onMessage = obj => games.push(obj);
+const printGames = () => {
+    for(let i = 0; i < games.length; i++) {
+        console.log(games[i]);
+    }
+};
 const onComplete = () => console.log('The stream has completed');
-
 
 // If fetching API endpoint succeeds, resolve the Fetch promise
 stream
 .then(readStream(onMessage))
+.then(printGames)
 .then(onComplete);
