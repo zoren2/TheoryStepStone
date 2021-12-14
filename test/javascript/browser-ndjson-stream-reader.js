b1c80e97f8adb7f3f4eed3392a42d634331de8c4
Jack Tsang <cic_tian2@yahoo.com>
2021-12-13 16:06:56 -0800

    Created object methods to parse game data

--- test/javascript/browser-ndjson-stream-reader.js
+++ test/javascript/browser-ndjson-stream-reader.js
@@ -1,68 +1,92 @@
     /*
-     * Utility script adapted by browser-ndjson-stream-reader by 
-     * Thibault Duplessis / ornicar@github
-     * https://gist.github.com/ornicar/a097406810939cf7be1df8ea30e94f3e
-     */ 
+     * This script contains the logic which will process three recently played games filtered by user
+     * defined restrictions (by time control) and allow the user to learn from their chess opening mistakes by 
+     * choosing suggestions offered by the lichess cloud. If no suggestions can be made then the user has succeeded
+     * in surviving the opening phase within playable bounds of browser-based Stockfish.
+     * 
+     * In order to use this script, create a new TheoryStepStone object 
+     * 
+     * eg: const theoryStepStone = new TheoryStepStone('user')
+     * 
+     * in user defined javasScript or inside HTML script and use the API granted by the object methods.
+     */
 
-    //onMessage function is passed to readStream
-    //onMessage is a callback function which just prints a line
-    const readStream = processLine => response => {
-        // 
-        const stream = response.body.getReader();
-        const matcher = /\r?\n/;
 
-        // A decoder takes a stream of bytes as input and emits a stream of code points
-        const decoder = new TextDecoder();
+    class TheoryStepStone {
+        constructor(userName) {
+            const stream = fetch(`https://lichess.org/api/games/user/${userName}?max=3`,{headers:{Accept:'application/x-ndjson'}});
+            const pushGames = (obj) => this.games.push(obj);
+            this.games = new Array();
+            this.userName = userName;
+            this.errorMessage = '';
+            stream.then(this.readStream(pushGames)); // readStream handles and populates the user games array
+            console.log(this.games);
+        }
+ 
+        getUserName() {
+            return this.userName;
+        }
 
-        let buf = '';
+        // This method requires the game index eg: games[1] to access the index.
+        getCurrentOpponent(game) {
 
-        const loop = () =>
-            // read() returns a promise that resolves when a value has been received
-            stream.read().then(
-                // Result objects contain two properties:
-                // done  - true if the stream has already given you all its data.
-                // value - some data. Always undefined when done is true.
-                ({ done, value }) => {
-                if (done) {
-                    if (buf.length > 0) processLine(JSON.parse(buf));
-                }      
+        }
 
-                // Not finished, populate the buffer with stream content
-                else {
-                    const chunk = decoder.decode(value, {
-                        stream: true
-                    });         
-                    buf += chunk;
+        // Returns whether or not the user is White or Black in order to orient the board properly.
+        getCurrentPlayerIsWhite(game){
 
-                    // Splits the content in order to display it
-                    const parts = buf.split(matcher);
-                    buf = parts.pop();
+        }
+
+        /*
+         * NDJson script adapted by browser-ndjson-stream-reader by 
+         * Thibault Duplessis / ornicar@github
+         * https://gist.github.com/ornicar/a097406810939cf7be1df8ea30e94f3e
+         */ 
+        readStream(processLine) {
+            return function (response) {
+                const stream = response.body.getReader();
+                const matcher = /\r?\n/;
+
+                // A decoder takes a stream of bytes as input and emits a stream of code points
+                const decoder = new TextDecoder();
+
+                let buf = '';
+
+                const loop = () =>
+                    // read() returns a promise that resolves when a value has been received
+                    stream.read().then(
+                    // Result objects contain two properties:
+                    // done  - true if the stream has already given you all its data.
+                    // value - some data. Always undefined when done is true.
+                    ({ done, value }) => {
+                        if (done) {
+                            if (buf.length > 0) processLine(JSON.parse(buf));
+                        }      
+
+                        // Not finished, populate the buffer with stream content
+                        else {
+                            const chunk = decoder.decode(value, {
+                            stream: true
+                        });         
+                        buf += chunk;
+
+                        // Splits the content in order to display it
+                        const parts = buf.split(matcher);
+                        buf = parts.pop();
                     
-                    // This segment
-                    for (const i of parts.filter(p => p)) processLine(JSON.parse(i));
-                    return loop();
-                    }
+                        for (const i of parts.filter(p => p)) processLine(JSON.parse(i));
+                            return loop();
+                        }
             });
 
-        // Read from the stream until it's complete
-        return loop();
-    }
-// const stream = fetch('https://lichess.org/api/tv/feed');
-// or any other ND-JSON endpoint such as:
- 
-// fetch() returns a Promise that resolves to a Response Interface
-const stream = fetch('https://lichess.org/api/games/user/lovlas?max=5',{headers:{Accept:'application/x-ndjson'}});
-console.log(stream);
-const games = new Array();
-// Callback function that simply logs a passed in object
-const onMessage = obj => games.push(obj);
-const printGames = () => {
-    games.forEach(game => console.log(game));
-};
-const onComplete = () => console.log('The stream has completed');
+            //Read from the stream until it's complete
+            return loop();
+            }
+        } // End fn
 
-// If fetching API endpoint succeeds, resolve the Fetch promise
-stream
-.then(readStream(onMessage))
-.then(printGames)
-.then(onComplete);
+        // Placeholder to validate user name
+        validateUser(userName) {
+
+        }
+    } // End class
+      
